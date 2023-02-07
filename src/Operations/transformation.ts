2eb8667b5336b71ef9c9862fffe5a3a185c00d6c
rayhankinan <rayhankinan@gmail.com>
2023-02-07 15:51:36 +0700

    Matrix - coordinate multiplication

---
+++ src/Operations/transformation.ts
@@ -0,0 +1,6 @@
+import Coordinate from "Operations/coordinate";
+import Point from "Operations/point";
+import Vector from "Operations/vector";
+import Matrix from "Operations/matrix";
+
+class Transformation {}
--- src/utils/resize-canvas.ts
+++ src/utils/resize-canvas.ts
@@ -2,9 +2,6 @@ function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement) {
   const width = canvas.clientWidth;
   const height = canvas.clientHeight;
 
-  console.log(width);
-  console.log(height);
-
   canvas.width = width;
   canvas.height = height;
 }
