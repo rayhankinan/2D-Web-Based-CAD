df708021e236292f81cd1fe00895097e8fdbca3e
rayhankinan <rayhankinan@gmail.com>
2023-02-07 16:43:51 +0700

    Add general transformation

--- src/Operations/transformation.ts
+++ src/Operations/transformation.ts
@@ -1,6 +1,72 @@
-import Coordinate from "Operations/coordinate";
 import Point from "Operations/point";
 import Vector from "Operations/vector";
 import Matrix from "Operations/matrix";
 
-class Transformation {}
+class Transformation {
+  static translation(p: Point, tx: number, ty: number): Point {
+    /* Create transformation matrix */
+    const v1 = new Vector([1, 0]);
+    const v2 = new Vector([0, 1]);
+    const pivot = new Point([tx, ty]);
+    const matrix = new Matrix([v1, v2, pivot]);
+
+    /* Count result point */
+    const newPoint = matrix.multiplyCoordinate(p) as Point;
+
+    return newPoint;
+  }
+
+  static rotation(p: Point, degree: number): Point {
+    /* Create transformation matrix */
+    const v1 = new Vector([Math.cos(degree), Math.sin(degree)]);
+    const v2 = new Vector([-Math.sin(degree), Math.cos(degree)]);
+    const pivot = new Vector([0, 0]);
+    const matrix = new Matrix([v1, v2, pivot]);
+
+    /* Count result point */
+    const newPoint = matrix.multiplyCoordinate(p) as Point;
+
+    return newPoint;
+  }
+
+  static scale(p: Point, sx: number, sy: number): Point {
+    /* Create transformation matrix */
+    const v1 = new Vector([sx, 0]);
+    const v2 = new Vector([0, sy]);
+    const pivot = new Point([0, 0]);
+    const matrix = new Matrix([v1, v2, pivot]);
+
+    /* Count result point */
+    const newPoint = matrix.multiplyCoordinate(p) as Point;
+
+    return newPoint;
+  }
+
+  static shearX(p: Point, kx: number): Point {
+    /* Create transformation matrix */
+    const v1 = new Vector([1, kx]);
+    const v2 = new Vector([0, 1]);
+    const pivot = new Point([0, 0]);
+    const matrix = new Matrix([v1, v2, pivot]);
+
+    /* Count result point */
+    const newPoint = matrix.multiplyCoordinate(p) as Point;
+
+    return newPoint;
+  }
+
+  static shearY(p: Point, ky: number) {
+    /* Create transformation matrix */
+    const v1 = new Vector([1, 0]);
+    const v2 = new Vector([ky, 1]);
+    const pivot = new Point([0, 0]);
+    const matrix = new Matrix([v1, v2, pivot]);
+
+    /* Count result point */
+    const newPoint = matrix.multiplyCoordinate(p) as Point;
+
+    return newPoint;
+  }
+}
+
+export default Transformation;
